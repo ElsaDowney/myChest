c64d2d89c2565087ab392a08510cd629b7a9e11b
xiaopanglzhu <1522898103@qq.com>
2016-08-14 17:37:29 +0800

    add match to db

--- routes/actions/matchAction.js
+++ routes/actions/matchAction.js
@@ -1,12 +1,16 @@
 const match = require('../models/match');
+
 exports.updateMatches = function(req,res){
-  //  match.updateMatches(req.body,function(result){
-  //    res.json(result).end();
-  //  });
-  res.json('...').end();
+  const _id = req.body._id;
+  const matches = req.body.matches;
+  const c_id1 = parseInt(matches[0]);
+  const c_id2 = parseInt(matches[1]);
+   match.updateMatches(_id,c_id1,c_id2,function(result){
+   });
+   match.updateMatches(_id,c_id2,c_id1,function(result){
+   });
+   res.json("ok").end();
 };
-
-
 
 
 exports.AllMatches = function(req,res){
